d20a3df7f81c0bff236e5ebc3bff77de22b4b038
Frida Bergströmn <fb223es@student.lnu.se>
2026-04-22 16:41:34 +0200

    ui

---
+++ src/ui/graphic/Platform.js
@@ -0,0 +1,15 @@
+/**
+ * Klass för plattformar
+ * 
+ */
+function Plattform(x, y, heigt, length){
+    this.x = x;
+    this.y = y;
+    this. heigt = heigt;
+    this.length = length;
+
+    //construktor
+
+    //min fil rör inte kamske om jag inte behöver hjälp
+    
+}
