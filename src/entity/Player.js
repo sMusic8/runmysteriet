bd33af5b967f28af51bd2cd2630f4c5e32dd517d
Frida Bergströmn <fb223es@student.lnu.se>
2026-04-24 11:34:07 +0200

    Sprites

--- src/entity/Player.js
+++ src/entity/Player.js
@@ -2,18 +2,22 @@
 // PLAYER
 //------------------------------------------------------------------------------
 
-runmysteriet.entity.Player = function(controls) {
+runmysteriet.entity.Player = function(controls, spriteConfig) {
 
-    rune.display.Graphic.call(this,
+    // 🔁 ÄNDRING: Graphic → Sprite (krävs för animationer)
+    rune.display.Sprite.call(this,
         0,
         0,
         32,
         32,
-        "start"
+        spriteConfig.texture // 🔥 HÄR används bilden du skickar in från Game
     );
 
-    // Kontroller
+    // Kontroller (vänster/höger/hopp)
     this.controls = controls;
+
+    // 🔁 NYTT: sparar animation-data
+    this.spriteConfig = spriteConfig;
 
     // Y-hastighet
     this.velocityY = 0;
@@ -32,7 +36,7 @@ runmysteriet.entity.Player = function(controls) {
 };
 
 // Inheritance
-runmysteriet.entity.Player.prototype = Object.create(rune.display.Graphic.prototype);
+runmysteriet.entity.Player.prototype = Object.create(rune.display.Sprite.prototype);
 runmysteriet.entity.Player.prototype.constructor = runmysteriet.entity.Player;
 
 //------------------------------------------------------------------------------
@@ -41,10 +45,18 @@ runmysteriet.entity.Player.prototype.constructor = runmysteriet.entity.Player;
 
 runmysteriet.entity.Player.prototype.init = function() {
 
-    rune.display.Graphic.prototype.init.call(this);
+    rune.display.Sprite.prototype.init.call(this);
 
     this.groundY = this.y;
     this.isOnGround = true;
+
+    // 🔁 NYTT: Lägg till animationer från config
+    for (var i = 0; i < this.spriteConfig.animations.length; i++) {
+        this.animation.add(this.spriteConfig.animations[i]);
+    }
+
+    // 🔁 Start-animation
+    this.animation.play(this.spriteConfig.start);
 };
 
 //------------------------------------------------------------------------------
@@ -53,20 +65,34 @@ runmysteriet.entity.Player.prototype.init = function() {
 
 runmysteriet.entity.Player.prototype.handleInput = function() {
 
+    var moving = false;
+
     // Höger
     if (this.keyboard.pressed(this.controls.right)) {
         this.x += 4;
+        moving = true;
     }
 
     // Vänster
     if (this.keyboard.pressed(this.controls.left)) {
         this.x -= 4;
+        moving = true;
     }
 
     // Hopp
     if (this.keyboard.pressed(this.controls.jump) && this.isOnGround) {
-
         this.velocityY = this.jumpPower;
         this.isOnGround = false;
+    }
+
+    // 🔁 NYTT: Animation baserat på rörelse
+    if (!this.isOnGround) {
+        this.animation.play("jump");
+    }
+    else if (moving) {
+        this.animation.play("run");
+    }
+    else {
+        this.animation.play("idle");
     }
 };
--- src/scene/game/Game.js
+++ src/scene/game/Game.js
@@ -6,7 +6,6 @@ runmysteriet.scene.Game = function() {
 
     rune.scene.Scene.call(this);
 
-    // 🔁 ÄNDRING: flera spelare istället för en
     this.m_players = [];
 
     this.r_bana1 = null;
@@ -25,42 +24,88 @@ runmysteriet.scene.Game.prototype.init = function() {
 
     rune.scene.Scene.prototype.init.call(this);
 
-    // 🔁 ÄNDRING: skapa två spelare med olika kontroller
-    var player1 = new runmysteriet.entity.Player({
-        left: "LEFT",
-        right: "RIGHT",
-        jump: "UP"
-    });
+    //==============================================================================
+    // 🔥 HÄR SÄTTER DU DINA BILDER (SPRITESHEETS)
+    //==============================================================================
+
+    var player1 = new runmysteriet.entity.Player(
+
+        // 🎮 Kontroller
+        {
+            left: "LEFT",
+            right: "RIGHT",
+            jump: "UP"
+        },
+
+        // 🖼️ SPRITE CONFIG
+        {
+            texture: "spritesheet_freya_move", // 🔥 ← NAMNET från din asset-loader
+
+            start: "idle",
+
+            animations: [
+                new rune.animation.Animation("idle", [0], 5, true),
+                new rune.animation.Animation("run", [1,2,3,4], 10, true),
+                new rune.animation.Animation("jump", [5], 1, false)
+            ]
+        }
+    );
+
     player1.x = 0;
     player1.y = 180;
 
-    var player2 = new runmysteriet.entity.Player({
-        left: "A",
-        right: "D",
-        jump: "W"
-    });
+
+    //==============================================================================
+    // PLAYER 2 (egen bild!)
+    //==============================================================================
+
+    var player2 = new runmysteriet.entity.Player(
+
+        {
+            left: "A",
+            right: "D",
+            jump: "W"
+        },
+
+        {
+            texture: "spritesheet_thor_move", // 🔥 ← ANDRA bilden
+
+            start: "idle",
+
+            animations: [
+                new rune.animation.Animation("idle", [0], 5, true),
+                new rune.animation.Animation("run", [1,2,3,4], 10, true),
+                new rune.animation.Animation("jump", [5], 1, false)
+            ]
+        }
+    );
+
     player2.x = 100;
     player2.y = 180;
 
-    // 🔁 Lägg in i array
+
+    // Lägg i array
     this.m_players.push(player1);
     this.m_players.push(player2);
 
-    // Platform 1
+
+    //==============================================================================
+    // PLATTFORMAR (oförändrat – rätt design)
+    //==============================================================================
+
     this.r_bana1 = new runmysteriet.ui.Platform();
     this.r_bana1.x = 200;
     this.r_bana1.y = 180;
 
-    // Platform 2
     this.r_bana2 = new runmysteriet.ui.Platform();
     this.r_bana2.x = 300;
     this.r_bana2.y = 180;
 
-    // Add to stage
+
+    // Lägg till i scen
     this.stage.addChild(this.r_bana1);
     this.stage.addChild(this.r_bana2);
 
-    // 🔁 Lägg till alla spelare
     for (var i = 0; i < this.m_players.length; i++) {
         this.stage.addChild(this.m_players[i]);
     }
@@ -74,37 +119,28 @@ runmysteriet.scene.Game.prototype.update = function(step) {
 
     rune.scene.Scene.prototype.update.call(this, step);
 
-    // 🔁 Loopa igenom alla spelare (INGEN duplicerad kod)
     for (var i = 0; i < this.m_players.length; i++) {
 
         var player = this.m_players[i];
 
-        // Input
         player.handleInput();
 
-        // Reset ground state
         player.isOnGround = false;
 
-        // Gravitation
         player.velocityY += player.gravity;
-
-        // Rörelse
         player.y += player.velocityY;
 
-        var isOnAnyPlatform = false;
+        var onPlatform = false;
 
-        // Kollisioner
         if (this.checkPlatform(player, this.r_bana1)) {
-            isOnAnyPlatform = true;
+            onPlatform = true;
         }
 
         if (this.checkPlatform(player, this.r_bana2)) {
-            isOnAnyPlatform = true;
+            onPlatform = true;
         }
 
-        // Fallback mark
-        if (player.y >= player.groundY && !isOnAnyPlatform) {
-
+        if (player.y >= player.groundY && !onPlatform) {
             player.y = player.groundY;
             player.velocityY = 0;
             player.isOnGround = true;
@@ -122,7 +158,6 @@ runmysteriet.scene.Game.prototype.checkPlatform = function(player, platform) {
         return false;
     }
 
-    // bara om spelaren faller nedåt
     if (player.velocityY >= 0 && player.y < platform.y) {
 
         player.y = platform.y - player.height;
