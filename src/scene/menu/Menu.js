e1ffbec0261b8ff037a8f56e57fa27f1839421f5
Frida Bergströmn <fb223es@student.lnu.se>
2026-04-28 13:37:30 +0200

    meny

--- src/scene/menu/Menu.js
+++ src/scene/menu/Menu.js
@@ -3,27 +3,26 @@
 //------------------------------------------------------------------------------
 
 /**
- * Creates a new object.
+ * Menu scene constructor.
  *
  * @constructor
  * @extends rune.scene.Scene
- *
- * @class
- * @classdesc
- * 
- * 
- * Menu scene.
  */
 runmysteriet.scene.Menu = function() {
 
-    //--------------------------------------------------------------------------
-    // Super call
-    //--------------------------------------------------------------------------
-    
-    /**
-     * Calls the constructor method of the super class.
-     */
     rune.scene.Scene.call(this);
+
+    /**
+     * List of menu item bitmap fields.
+     * @type {Array<rune.text.BitmapField>}
+     */
+    this.menuItems = [];
+
+    /**
+     * Currently selected menu index.
+     * @type {number}
+     */
+    this.selectedIndex = 0;
 };
 
 //------------------------------------------------------------------------------
@@ -34,63 +33,146 @@ runmysteriet.scene.Menu.prototype = Object.create(rune.scene.Scene.prototype);
 runmysteriet.scene.Menu.prototype.constructor = runmysteriet.scene.Menu;
 
 //------------------------------------------------------------------------------
-// Override public prototype methods (ENGINE)
+// Init
 //------------------------------------------------------------------------------
 
 /**
- * This method is automatically executed once after the scene is instantiated. 
- * The method is used to create objects to be used within the scene.
+ * Initializes the scene and creates menu items.
  *
- * @returns {undefined}
+ * @return {undefined}
  */
 runmysteriet.scene.Menu.prototype.init = function() {
 
     rune.scene.Scene.prototype.init.call(this);
-    
-    // första raden
-    var text = new rune.text.BitmapField("Hello Menu!");
+
+    var text = new rune.text.BitmapField("Valkommen till runmysteriet!");
     text.autoSize = true;
     text.center = this.application.screen.center;
-    text.y -= 15; // 🔥 flytta upp
-text.flicker.start(1000, 0.5);
-    
+    text.y -= 40;
+    text.flicker.start(750, 0.5);
     this.stage.addChild(text);
 
-    // andra raden
-    var text2 = new rune.text.BitmapField("Hello player!");
+    var text2 = new rune.text.BitmapField("From battle to brain, earn the final word!");
     text2.autoSize = true;
     text2.center = this.application.screen.center;
-    text2.y += 15; // 🔥 flytta ner
-    
+    text2.y -= 20;
+    text2.scale = 0.8;
+    text2.flicker.start(750, 0.5);
     this.stage.addChild(text2);
+
+    // -------------------------
+    // MENY
+    // -------------------------
+
+    var labels = ["Starta spelet", "Las mer", "Credits"];
+
+    for (var i = 0; i < labels.length; i++) {
+
+        var item = new rune.text.BitmapField(labels[i]);
+        item.autoSize = true;
+        item.center = this.application.screen.center;
+        item.y += 40 + (i * 15);
+        item.scale = 0.8;
+
+        this.stage.addChild(item);
+        this.menuItems.push(item);
+    }
+
+    this.updateMenu();
 };
 
+//------------------------------------------------------------------------------
+// Update
+//------------------------------------------------------------------------------
+
 /**
- * This method is automatically executed once per "tick". The method is used for 
- * calculations such as application logic.
+ * Updates the scene logic each frame.
  *
  * @param {number} step Fixed time step.
- *
- * @returns {undefined}
+ * @return {undefined}
  */
 runmysteriet.scene.Menu.prototype.update = function(step) {
+
     rune.scene.Scene.prototype.update.call(this, step);
 
-    if(this.keyboard.justPressed("SPACE")) {
-        this.application.scenes.load([
-            new runmysteriet.scene.Game()
-        ])
+    // ↓
+    if (this.keyboard.justPressed("DOWN")) {
+
+        this.selectedIndex++;
+
+        if (this.selectedIndex >= this.menuItems.length) {
+            this.selectedIndex = 0;
+        }
+
+        this.updateMenu();
+    }
+
+    // ↑
+    if (this.keyboard.justPressed("UP")) {
+
+        this.selectedIndex--;
+
+        if (this.selectedIndex < 0) {
+            this.selectedIndex = this.menuItems.length - 1;
+        }
+
+        this.updateMenu();
+    }
+
+    // SPACE = valj
+    if (this.keyboard.justPressed("SPACE")) {
+
+        if (this.selectedIndex === 0) {
+            this.application.scenes.load([
+                new runmysteriet.scene.Game()
+            ]);
+        }
+
+        if (this.selectedIndex === 1) {
+            console.log("Öppna Las mer");
+        }
+
+        if (this.selectedIndex === 2) {
+            console.log("Öppna Credits");
+        }
     }
 };
 
+//------------------------------------------------------------------------------
+// Update menu UI
+//------------------------------------------------------------------------------
+
 /**
- * This method is automatically called once just before the scene ends. Use 
- * the method to reset references and remove objects that no longer need to 
- * exist when the scene is destroyed. The process is performed in order to 
- * avoid memory leaks.
+ * Updates visual selection state of menu items.
  *
- * @returns {undefined}
+ * @return {undefined}
+ */
+runmysteriet.scene.Menu.prototype.updateMenu = function() {
+
+    for (var i = 0; i < this.menuItems.length; i++) {
+
+        var item = this.menuItems[i];
+
+        var text = item.text.replace("> ", "");
+
+        if (i === this.selectedIndex) {
+            item.text = "> " + text;
+        } else {
+            item.text = text;
+        }
+    }
+};
+
+//------------------------------------------------------------------------------
+// Dispose
+//------------------------------------------------------------------------------
+
+/**
+ * Cleans up the scene before destruction.
+ *
+ * @return {undefined}
  */
 runmysteriet.scene.Menu.prototype.dispose = function() {
+
     rune.scene.Scene.prototype.dispose.call(this);
 };
