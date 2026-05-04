da551f9cbb91561992ac6f85615af3b02f123f10
Frida Bergströmn <fb223es@student.lnu.se>
2026-05-04 20:55:48 +0200

    Kan stå på varandra

--- src/handler/PlayerHandler.js
+++ src/handler/PlayerHandler.js
@@ -1,7 +1,8 @@
 //------------------------------------------------------------------------------
-// PLAYER HANDLER (RENSAD)
+// PLAYER HANDLER
 //------------------------------------------------------------------------------
 
+//Klassen där alla spelare i spelet hanteras.
 runmysteriet.handler.PlayerHandler = function(stage, platforms, application) {
 
     this.stage = stage;
@@ -34,49 +35,39 @@ runmysteriet.handler.PlayerHandler.prototype.init = function() {
     player2.x = 100;
     player2.y = 188;
 
-    this.setupPlayer(player1);
-    this.setupPlayer(player2);
+    // HP INIT
+    player1.hp = 100;
+    player1.maxHp = 100;
 
+    player2.hp = 100;
+    player2.maxHp = 100;
+
+    // PHYSICS INIT
+    player1.previousY = player1.y;
+    player2.previousY = player2.y;
+
+    player1.velocityY = 0;
+    player2.velocityY = 0;
+
+    player1.gravity = 0.5;
+    player2.gravity = 0.5;
+
+    player1.isOnGround = false;
+    player2.isOnGround = false;
+
+    this.players.push(player1);
+    this.players.push(player2);
+
+    // HP BARS
     player1.hpBar = this.createHpBar();
     player2.hpBar = this.createHpBar();
 
     this.stage.addChild(player1.hpBar);
     this.stage.addChild(player2.hpBar);
 
-    this.players.push(player1);
-    this.players.push(player2);
-
     for (var i = 0; i < this.players.length; i++) {
         this.stage.addChild(this.players[i]);
     }
-};
-
-//------------------------------------------------------------------------------
-// SETUP PLAYER
-//------------------------------------------------------------------------------
-
-runmysteriet.handler.PlayerHandler.prototype.setupPlayer = function(player) {
-
-    player.hp = 100;
-    player.maxHp = 100;
-
-    player.hitCooldown = 0;
-    player.velocityY = 0;
-    player.gravity = 0.5;
-    player.isOnGround = false;
-};
-
-//------------------------------------------------------------------------------
-// HP BAR
-//------------------------------------------------------------------------------
-
-runmysteriet.handler.PlayerHandler.prototype.createHpBar = function() {
-
-    var bar = new rune.display.Sprite(0, 0, 32, 4, "hpbar");
-    bar.anchorX = 0;
-    bar.scaleX = 1;
-
-    return bar;
 };
 
 //------------------------------------------------------------------------------
@@ -85,7 +76,9 @@ runmysteriet.handler.PlayerHandler.prototype.createHpBar = function() {
 
 runmysteriet.handler.PlayerHandler.prototype.update = function() {
 
-    this.applyPhysics();
+    this.updateInput();
+    this.updateMovement();
+    this.updateCollisions();
 
     for (var i = 0; i < this.players.length; i++) {
 
@@ -103,38 +96,85 @@ runmysteriet.handler.PlayerHandler.prototype.update = function() {
             p.hpBar.scaleX = hpPercent;
         }
 
-        // COOLDOWN
-        if (p.hitCooldown > 0) {
-            p.hitCooldown--;
-        }
-
-        // INPUT
-        this.handleInput(p, i);
-
-        // ANIMATION
         p.updateAnimation();
     }
 };
 
 //------------------------------------------------------------------------------
-// PHYSICS
+// INPUT
 //------------------------------------------------------------------------------
 
-runmysteriet.handler.PlayerHandler.prototype.applyPhysics = function() {
+runmysteriet.handler.PlayerHandler.prototype.updateInput = function() {
 
     for (var i = 0; i < this.players.length; i++) {
 
-        var p = this.players[i];
+        var player = this.players[i];
 
-        p.previousY = p.y;
+        player.previousY = player.y;
+        player.isMoving = false;
 
-        p.velocityY += p.gravity;
-        p.y += p.velocityY;
+        this.handleInput(player, i);
+    }
+};
 
-        p.isOnGround = false;
+//------------------------------------------------------------------------------
+// MOVEMENT
+//------------------------------------------------------------------------------
 
+runmysteriet.handler.PlayerHandler.prototype.updateMovement = function() {
+
+    for (var i = 0; i < this.players.length; i++) {
+
+        var player = this.players[i];
+
+        player.velocityY += player.gravity;
+        player.y += player.velocityY;
+
+        player.isOnGround = false;
+    }
+};
+
+//------------------------------------------------------------------------------
+// COLLISIONS
+//------------------------------------------------------------------------------
+
+runmysteriet.handler.PlayerHandler.prototype.updateCollisions = function() {
+
+    for (var i = 0; i < this.players.length; i++) {
+
+        var player = this.players[i];
+
+        var onPlatform = false;
+
+        // PLATFORMER
         for (var j = 0; j < this.platforms.length; j++) {
-            this.checkPlatform(p, this.platforms[j]);
+
+            var platform = this.platforms[j];
+
+            if (Math.abs(platform.x - player.x) > 350) continue;
+
+            if (this.checkPlatform(player, platform)) {
+                onPlatform = true;
+            }
+        }
+
+        // PLAYER ON PLAYER (STÅ PÅ VARANDRA)
+        for (var k = 0; k < this.players.length; k++) {
+
+            var other = this.players[k];
+
+            if (player === other) continue;
+
+            if (this.checkPlayerPlatform(player, other)) {
+                onPlatform = true;
+            }
+        }
+
+        // GROUND
+        if (player.y >= player.groundY && !onPlatform) {
+            player.y = player.groundY;
+            player.velocityY = 0;
+            player.isOnGround = true;
         }
     }
 };
@@ -162,7 +202,42 @@ runmysteriet.handler.PlayerHandler.prototype.checkPlatform = function(player, pl
 };
 
 //------------------------------------------------------------------------------
-// INPUT
+// PLAYER ON PLAYER (VIKTIG DEL)
+//------------------------------------------------------------------------------
+
+runmysteriet.handler.PlayerHandler.prototype.checkPlayerPlatform = function(player, other) {
+
+    var hitboxOffsetX = 10;
+    var hitboxWidth = other.width - 20;
+
+    var playerPreviousBottom = player.previousY + player.height;
+    var otherTop = other.y;
+
+    var otherLeft = other.x + hitboxOffsetX;
+    var otherRight = other.x + hitboxOffsetX + hitboxWidth;
+
+    var playerCenterX = player.x + player.width / 2;
+
+    var isOverOther =
+        playerCenterX >= otherLeft &&
+        playerCenterX <= otherRight;
+
+    var wasAbove = playerPreviousBottom <= otherTop;
+
+    if (player.velocityY >= 0 && wasAbove && isOverOther) {
+
+        player.y = otherTop - player.height;
+        player.velocityY = 0;
+        player.isOnGround = true;
+
+        return true;
+    }
+
+    return false;
+};
+
+//------------------------------------------------------------------------------
+// INPUT HANDLER
 //------------------------------------------------------------------------------
 
 runmysteriet.handler.PlayerHandler.prototype.handleInput = function(player, gamepadID) {
@@ -186,10 +261,21 @@ runmysteriet.handler.PlayerHandler.prototype.handleInput = function(player, game
         player.velocityY = player.jumpPower;
         player.isOnGround = false;
 
-        if (this.jumpSound) {
-            this.jumpSound.play();
-        }
+        if (this.jumpSound) this.jumpSound.play();
     }
 
     player.isMoving = moving;
+};
+
+//------------------------------------------------------------------------------
+// HP BAR
+//------------------------------------------------------------------------------
+
+runmysteriet.handler.PlayerHandler.prototype.createHpBar = function() {
+
+    var bar = new rune.display.Sprite(0, 0, 32, 4, "hpbar");
+    bar.anchorX = 0;
+    bar.scaleX = 1;
+
+    return bar;
 };
