0fe9116ae5b1be5351af9e654490c1771a2f6e13
sMusic8 <musicsabina1@gmail.com>
2026-05-04 15:44:09 +0200

    hål

--- src/handler/PlatformHandler.js
+++ src/handler/PlatformHandler.js
@@ -11,8 +11,19 @@ runmysteriet.handler.PlatformHandler = function(stage, screenWidth) {
 
     this.platforms = [];
 
-    this.tileSize = 268;
-    this.groundY = 220;
+    this.tileSize = 268;//storleken på varje plattform, används för att skapa plattformar i init funktionen
+    this.groundY = 220;//y-positionen där plattformarna ska placeras, används i init funktionen
+    this.holes = [
+        { x: 500, width: 100},
+        { x: 900, width: 90},
+        { x: 1300, width: 80},
+        { x: 1700, width: 70},
+        { x: 2100, width: 60},
+
+
+
+    ];
+
 };
 
 //------------------------------------------------------------------------------
@@ -22,6 +33,9 @@ runmysteriet.handler.PlatformHandler = function(stage, screenWidth) {
 runmysteriet.handler.PlatformHandler.prototype.init = function() {
 
     for (var x = 0; x < this.levelWidth; x += this.tileSize) {
+        if(this.holeInPlatform(x)){
+            continue;//om det finns ett hål i plattformen ska hoppa över att så det inte skapas en plattform där
+        }
 
         var platform = new runmysteriet.ui.Platform();
 
@@ -32,3 +46,22 @@ runmysteriet.handler.PlatformHandler.prototype.init = function() {
         this.stage.addChild(platform);
     }
 };
+
+runmysteriet.handler.PlatformHandler.prototype.holeInPlatform = function(platformX) {
+    var platformStart = platformX;
+    var platformEnd = platformX + this.tileSize;
+  
+    for (var i = 0; i < this.holes.length; i++) {
+
+        var hole = this.holes[i];
+        var holeStart = hole.x;
+        var holeEnd = hole.x + hole.width;
+        if (platformStart < holeEnd && platformEnd > holeStart) {
+            return true;
+        }
+
+    }
+
+
+
+}
