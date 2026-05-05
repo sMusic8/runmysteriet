94c02003c7a1fef61a6112dd7218efab9d8bc97b
Frida Bergströmn <fb223es@student.lnu.se>
2026-05-05 14:22:48 +0200

    gris i consollen

--- scripts/shell/build.sh
+++ scripts/shell/build.sh
@@ -14,6 +14,9 @@ npx google-closure-compiler \
 --js "./../../src/ui/graphic/Platform.js" \
 --js "./../../src/ui/graphic/Hole.js" \
 --js "./../../src/ui/graphic/Ground.js" \
+--js "./../../src/segments/Segment_1.js" \
+--js "./../../src/segments/Segment_2.js" \
+--js "./../../src/segments/Segment_3.js" \
 --js "./../../src/ui/graphic/Shield.js" \
 --js "./../../src/handler/BackgroundHandler.js" \
 --js "./../../src/handler/ShieldHandler.js" \
@@ -25,9 +28,7 @@ npx google-closure-compiler \
 --js "./../../src/scene/menu/Paus.js" \
 --js "./../../src/scene/menu/More.js" \
 --js "./../../src/scene/menu/Credits.js" \
---js "./../../src/segments/Segment_1.js" \
---js "./../../src/segments/Segment_2.js" \
---js "./../../src/segments/Segment_3.js" \
+
 --js "./../../src/scene/game/Game.js" \
 --js "./../../src/scene/menu/Menu.js" \
 --js "./../../src/system/Main.js" \
--- src/segments/Segment_1.js
+++ src/segments/Segment_1.js
@@ -1,3 +1,6 @@
 runmysteriet.segments.Segment_1 = function (){
 
 }
+runmysteriet.segments.Segment_1.prototype.ground = function(){
+    console.log("gris")
+}
