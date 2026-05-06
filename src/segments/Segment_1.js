54c9eeac76bb95e3137faa8b4249817a91b65da0
sMusic8 <musicsabina1@gmail.com>
2026-05-06 21:55:19 +0200

    ok

--- src/segments/Segment_1.js
+++ src/segments/Segment_1.js
@@ -1,25 +1,43 @@
 
 //segment 1 - första del av bana
+
+//------------------------------------------------------------------------------
+// SEGMENT 1
+//------------------------------------------------------------------------------
+
 runmysteriet.segments.Segment_1 = function() {
+    this.tileSize = 268;
     this.groundY = 220;
+    this.holeHeight = 200;
 };
+
+//------------------------------------------------------------------------------
+// GROUND
+//------------------------------------------------------------------------------
 //grounden i segmentet där skapas alla plattformar hål och fiender
+
 runmysteriet.segments.Segment_1.prototype.ground = function(stage, startX) {
-    
     var x = startX || 0;
 
-    var ground = new runmysteriet.ui.Platform();
-    ground.x = 0;
-    ground.y = this.groundY;
-    ground.width = 4000;   // gör den lång
-    stage.addChild(ground);
+    var platforms = [];
+    var holes = [];
+    var enemySpawns = [];
+    var waterAreas = [];
+    var movingPlatforms = [];
+    var boats = [];
+
+    //--------------------------------------------------------------------------
+    // FÖRSTA PLATTFORMEN
+    //--------------------------------------------------------------------------
 
     var platform1 = new runmysteriet.ui.Platform();
     platform1.x = x;
     platform1.y = this.groundY;
+
     stage.addChild(platform1);
     platforms.push(platform1);
 
+    // Enemy spawn på första plattformen
     enemySpawns.push({
         type: "kristen",
         x: x + 120,
@@ -28,38 +46,72 @@ runmysteriet.segments.Segment_1.prototype.ground = function(stage, startX) {
 
     x += this.tileSize;
 
+    //--------------------------------------------------------------------------
+    // ANDRA PLATTFORMEN
+    //--------------------------------------------------------------------------
+
     var platform2 = new runmysteriet.ui.Platform();
     platform2.x = x;
     platform2.y = this.groundY;
+
     stage.addChild(platform2);
     platforms.push(platform2);
 
     x += this.tileSize;
 
+    //--------------------------------------------------------------------------
+    // HÅL
+    //--------------------------------------------------------------------------
+
+    var holeX = x;
+
     var hole = new runmysteriet.ui.graphic.Hole(
-        x,
+        holeX,
         this.groundY,
-        60,
+        40,
         this.holeHeight
     );
 
-stage.addChild(platform);
+    stage.addChild(hole);
+    holes.push(hole);
+
+    //--------------------------------------------------------------------------
+    // LUFTPLATTFORM OVANFÖR HÅLET
+    //--------------------------------------------------------------------------
+
+    var airPlatform = new runmysteriet.ui.Platform();
+    airPlatform.x = holeX - 100;
+    airPlatform.y = this.groundY - 90;
+
+    stage.addChild(airPlatform);
+    platforms.push(airPlatform);
+
+    x += hole.width;
+
+    //--------------------------------------------------------------------------
+    // TREDJE PLATTFORMEN
+    //--------------------------------------------------------------------------
+
+    var platform3 = new runmysteriet.ui.Platform();
+    platform3.x = x;
+    platform3.y = this.groundY;
+
+    stage.addChild(platform3);
+    platforms.push(platform3);
+
+    x += this.tileSize;
+
+    //--------------------------------------------------------------------------
+    // RETURNERA SEGMENTETS DATA
+    //--------------------------------------------------------------------------
 
     return {
-        platforms: [ground],
-        holes: [],
-        enemySpawns: [],
-        endX: 4000
-    };
-};
-runmysteriet.segments.Segment_1.prototype.platforms = function(stage, startX) {
-
-
-
-    return {
-        platforms: [platform],
-        holes: [],
-        enemySpawns: [],
-        endX: x + 268
+        platforms: platforms,
+        holes: holes,
+        enemySpawns: enemySpawns,
+        waterAreas: waterAreas,
+        movingPlatforms: movingPlatforms,
+        boats: boats,
+        endX: x
     };
 };
