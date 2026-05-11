86bf355258acfba5ed5d89deaff3756d4773891d
Frida Bergströmn <fb223es@student.lnu.se>
2026-05-11 21:28:55 +0200

    Klar fil

--- src/scene/menu/Menu.js
+++ src/scene/menu/Menu.js
@@ -2,12 +2,11 @@
 // MENU SCENE
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu = function() {
+runmysteriet.scene.Menu = function () {
+  rune.scene.Scene.call(this);
 
-    rune.scene.Scene.call(this);
-
-    this.menuList = null;
-    this.menuSound = null;
+  this.menuList = null;
+  this.menuSound = null;
 };
 
 //------------------------------------------------------------------------------
@@ -21,120 +20,129 @@ runmysteriet.scene.Menu.prototype.constructor = runmysteriet.scene.Menu;
 // INIT
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu.prototype.init = function() {
+runmysteriet.scene.Menu.prototype.init = function () {
+  rune.scene.Scene.prototype.init.call(this);
 
-    rune.scene.Scene.prototype.init.call(this);
+  this.menuSound = this.application.sounds.sound.get("sound_menu");
 
-    this.menuSound = this.application.sounds.sound.get("sound_menu");
+  //----------------------------------------------------------------------
+  // MAIN TITLE (STÖRRE)
+  //----------------------------------------------------------------------
 
-    var text = new rune.text.BitmapField("Valkommen till runmysteriet!");
-    text.autoSize = true;
-    text.center = this.application.screen.center;
-    text.y -= 40;
-    text.flicker.start(750, 0.5);
-    this.stage.addChild(text);
+  var text = new rune.text.BitmapField("Welcome to the Rune Mystery");
+  text.autoSize = true;
+  text.center = this.application.screen.center;
+  text.y -= 70;
+  text.x -= 80;
+  text.scaleX = 2;
+  text.scaleY = 2;
 
-    var text2 = new rune.text.BitmapField(
-        "From battle to brain, earn the final word!"
-    );
-    text2.autoSize = true;
-    text2.center = this.application.screen.center;
-    text2.y -= 20;
-    text2.scale = 0.8;
-    text2.flicker.start(750, 0.5);
-    this.stage.addChild(text2);
+  text.flicker.start(750, 0.5);
 
-    this.menuList = new runmysteriet.ui.graphic.MenuList(
-        this.stage,
-        this.application,
-        ["Starta spelet", "Las mer", "Credits"],
-        40,
-        15,
-        0.8
-    );
+  this.stage.addChild(text);
+
+  //----------------------------------------------------------------------
+  // SUBTITLE (MINDRE)
+  //----------------------------------------------------------------------
+
+  var text2 = new rune.text.BitmapField(
+    "From battle to brain, earn the final word!",
+  );
+
+  text2.autoSize = true;
+  text2.center = this.application.screen.center;
+  text2.y -= 45;
+  text2.x -= 10;
+  //text2.scaleX = 0.8;
+  //text2.scaleY = 0.8;
+
+  text2.flicker.start(750, 0.5);
+
+  this.stage.addChild(text2);
+
+  //----------------------------------------------------------------------
+  // MENU (LAGOM STOR)
+  //----------------------------------------------------------------------
+
+  this.menuList = new runmysteriet.ui.graphic.MenuList(
+    this.stage,
+    this.application,
+    ["Play game", "Read more", "Credits"],
+    40,
+    15,
+    1, // ← var 0.8, nu lite större
+  );
+  this.menuList.scaleX = 2.5;
+  this.menuList.scaleY = 2.5;
 };
 
 //------------------------------------------------------------------------------
 // UPDATE
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu.prototype.update = function(step) {
+runmysteriet.scene.Menu.prototype.update = function (step) {
+  rune.scene.Scene.prototype.update.call(this, step);
 
-    rune.scene.Scene.prototype.update.call(this, step);
+  if (!this.menuList || typeof this.menuList.readInput !== "function") {
+    return;
+  }
 
-    if (!this.menuList || typeof this.menuList.readInput !== "function") {
-        return;
-    }
+  var input = this.menuList.readInput(this.keyboard);
 
-    var input = this.menuList.readInput(this.keyboard);
+  if (input.down) {
+    this.playMenuSound();
+    this.menuList.moveNext();
+  }
 
-    if (input.down) {
-        this.playMenuSound();
-        this.menuList.moveNext();
-    }
+  if (input.up) {
+    this.playMenuSound();
+    this.menuList.movePrevious();
+  }
 
-    if (input.up) {
-        this.playMenuSound();
-        this.menuList.movePrevious();
-    }
-
-    if (input.choose) {
-        this.chooseSelected();
-    }
+  if (input.choose) {
+    this.chooseSelected();
+  }
 };
 
 //------------------------------------------------------------------------------
 // CHOOSE
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu.prototype.chooseSelected = function() {
-
-    var selectedIndex = this.menuList.getSelectedIndex();
+runmysteriet.scene.Menu.prototype.chooseSelected = function () {
+  var selectedIndex = this.menuList.getSelectedIndex();
 
   if (selectedIndex === 0) {
-
     this.application.scenes.load([
-        new runmysteriet.scene.TextInputView(function() {
-
-            return " ";        })
+      new runmysteriet.scene.TextInputView(function () {
+        return " ";
+      }),
     ]);
-
-}
-    else if (selectedIndex === 1) {
-
-        this.application.scenes.load([
-            new runmysteriet.scene.More()
-        ]);
-    }
-    else if (selectedIndex === 2) {
-
-        this.application.scenes.load([
-            new runmysteriet.scene.Credits()
-        ]);
-    }
+  } else if (selectedIndex === 1) {
+    this.application.scenes.load([new runmysteriet.scene.More()]);
+  } else if (selectedIndex === 2) {
+    this.application.scenes.load([new runmysteriet.scene.Credits()]);
+  }
 };
 
 //------------------------------------------------------------------------------
 // SOUND
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu.prototype.playMenuSound = function() {
-
-    if (this.menuSound) {
-        this.menuSound.play();
-    }
+runmysteriet.scene.Menu.prototype.playMenuSound = function () {
+  if (this.menuSound) {
+    this.menuSound.play();
+  }
 };
 
 //------------------------------------------------------------------------------
 // DISPOSE
 //------------------------------------------------------------------------------
 
-runmysteriet.scene.Menu.prototype.dispose = function() {
+runmysteriet.scene.Menu.prototype.dispose = function () {
+  if (this.menuList) {
+    this.menuList.clear();
+    this.menuList = null;
+  }
 
-    if (this.menuList) {
-        this.menuList.clear();
-        this.menuList = null;
-    }
-
-    rune.scene.Scene.prototype.dispose.call(this);
+  rune.scene.Scene.prototype.dispose.call(this);
 };
