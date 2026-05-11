cfa7214a5873d2ce8383fa1ba792c3338f8c639c
Frida Bergströmn <fb223es@student.lnu.se>
2026-05-11 21:07:43 +0200

    Färdig fil

--- src/scene/menu/More.js
+++ src/scene/menu/More.js
@@ -1,11 +1,19 @@
 /**
  * More info scene.
  *
+ * Visar instruktioner + hur man går tillbaka till menyn.
+ *
  * @constructor
  * @extends {rune.scene.Scene}
  */
 runmysteriet.scene.More = function() {
     rune.scene.Scene.call(this);
+
+    /** @private */
+    this.m_backButton = null;
+
+    /** @private */
+    this.m_text = null;
 };
 
 //------------------------------------------------------------------------------
@@ -19,43 +27,80 @@ runmysteriet.scene.More.prototype.constructor = runmysteriet.scene.More;
 // INIT
 //------------------------------------------------------------------------------
 
-/**
- * Initializes the scene.
- *
- * @return {void}
- */
 runmysteriet.scene.More.prototype.init = function() {
 
     rune.scene.Scene.prototype.init.call(this);
 
-    /** @type {!rune.text.BitmapField} */
-    var text = new rune.text.BitmapField("This is the game where you help the Vikings reach their home ship while avoiding numerous obstacles. Jump across platforms, fight Christian priests, and avoid being captured by the English. To board the ship and sail on to the next level, you must guess the secret password. Collect the shields marked with runes along the way.");
-    text.autoSize = true;
-    text.center = this.application.screen.center;
+    //----------------------------------------------------------------------
+    // BOX
+    //----------------------------------------------------------------------
 
-    this.stage.addChild(text);
+    var boxWidth = 520;
+    var boxHeight = 300;
+
+    var box = new rune.display.Graphic(0, 0, boxWidth, boxHeight);
+    box.fill = true;
+    box.fillColor = "#ffffff";
+
+    box.x = this.application.screen.center.x - boxWidth / 2;
+    box.y = this.application.screen.center.y - boxHeight / 2;
+
+    this.stage.addChild(box);
+
+    //----------------------------------------------------------------------
+    // TEXT (inkl. instruktion för att gå tillbaka)
+    //----------------------------------------------------------------------
+
+    this.m_text = new rune.text.BitmapField(
+        "This is the game where you help the Vikings\n" +
+        "reach their home ship while avoiding obstacles.\n\n" +
+
+        "Jump across platforms, fight priests,\n" +
+        "and avoid being captured.\n\n" +
+
+        "To board the ship you must guess\n" +
+        "the secret password.\n\n" +
+
+        "Collect shields with runes along the way.\n\n" +
+
+        "< BACK\n" +
+        "Press ENTER / SPACE / ESC\n" +
+        "Gamepad: START or X"
+    );
+
+    this.m_text.autoSize = true;
+
+    this.stage.addChild(this.m_text);
+
+    // centrera text i box
+    this.m_text.x = box.x + (boxWidth - this.m_text.width) / 2;
+    this.m_text.y = box.y + (boxHeight - this.m_text.height) / 2;
+
+    //----------------------------------------------------------------------
+    // BACK BUTTON (visuell förstärkning)
+    //----------------------------------------------------------------------
+
+    this.m_backButton = new rune.text.BitmapField("BACK");
+    this.m_backButton.autoSize = true;
+
+    this.stage.addChild(this.m_backButton);
+
+    this.m_backButton.x = this.application.screen.center.x - this.m_backButton.width / 2;
+    this.m_backButton.y = box.y + boxHeight + 15;
 };
 
 //------------------------------------------------------------------------------
 // UPDATE
 //------------------------------------------------------------------------------
 
-/**
- * Updates scene logic.
- *
- * @param {number} step Fixed time step.
- * @return {void}
- */
 runmysteriet.scene.More.prototype.update = function(step) {
 
     rune.scene.Scene.prototype.update.call(this, step);
 
-    /** @type {?Object} */
     var keyboard = this.keyboard;
-
-    /** @type {?Object} */
     var gamepad = this.gamepads.get(0);
 
+    // Keyboard
     if (
         keyboard &&
         (
@@ -64,20 +109,28 @@ runmysteriet.scene.More.prototype.update = function(step) {
             keyboard.justPressed("SPACE")
         )
     ) {
-        this.application.scenes.load([
-            new runmysteriet.scene.Menu()
-        ]);
+        this.goToMenu();
     }
 
+    // Gamepad
     if (
         gamepad &&
         (
-            gamepad.justPressed(9) ||  // Options / Start
-            gamepad.justPressed(0)     // X-knappen
+            gamepad.justPressed(9) ||  // Start
+            gamepad.justPressed(0)     // X
         )
     ) {
-        this.application.scenes.load([
-            new runmysteriet.scene.Menu()
-        ]);
+        this.goToMenu();
     }
+};
+
+//------------------------------------------------------------------------------
+// NAVIGATION
+//------------------------------------------------------------------------------
+
+runmysteriet.scene.More.prototype.goToMenu = function() {
+
+    this.application.scenes.load([
+        new runmysteriet.scene.Menu()
+    ]);
 };
