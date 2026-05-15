5d0428a1500ffbd60fe102462faf8ff766cdd84b
Frida Bergströmn <fb223es@student.lnu.se>
2026-05-15 12:32:26 +0200

    Kortare banor

--- src/handler/PlatformHandler.js
+++ src/handler/PlatformHandler.js
@@ -2,7 +2,6 @@
 // PLATFORM HANDLER
 //------------------------------------------------------------------------------
 
-// Denna klassen hanterar alla plattformar i spelet, skapar och kontrolerar dess kolision med players. 
 runmysteriet.handler.PlatformHandler = function(stage, screenWidth) {
     this.stage = stage;
     this.screenWidth = screenWidth;
@@ -11,13 +10,13 @@ runmysteriet.handler.PlatformHandler = function(stage, screenWidth) {
     this.holes = [];
     this.enemySpawns = [];
     this.waterAreas = [];
-    this.boats = [];                                    
+    this.boats = [];
 
     this.levelWidth = 0;
 };
 
 //------------------------------------------------------------------------------
-// PLATFORM HANDLER INIT
+// INIT
 //------------------------------------------------------------------------------
 
 runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
@@ -27,16 +26,20 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
     this.holes = [];
     this.enemySpawns = [];
     this.waterAreas = [];
-    this.levelWidth = 0;
     this.boats = [];
 
     var x = 0;
 
-    //--------------------------------------------------------------------------
-    // SEGMENTS
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    // WATER + END SEGMENT
+    //----------------------------------------------------------------------
 
     var waterSegment = runmysteriet.segments.Segment_Water;
+    var endSegment = runmysteriet.segments.Segment_End;
+
+    //----------------------------------------------------------------------
+    // VANLIGA SEGMENT (VI TAR BARA 3 ST)
+    //----------------------------------------------------------------------
 
     var segments = [
         runmysteriet.segments.Segment_1,
@@ -47,11 +50,9 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
         runmysteriet.segments.Segment_6
     ];
 
-    var endSegment = runmysteriet.segments.Segment_End;
-
-    //--------------------------------------------------------------------------
-    // shuffle (alla segment används exakt en gång)
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    // SHUFFLE
+    //----------------------------------------------------------------------
 
     for (var i = segments.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -60,9 +61,15 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
         segments[j] = temp;
     }
 
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    // TA ENDAST 3 UNIKA SEGMENT
+    //----------------------------------------------------------------------
+
+    var chosenSegments = segments.slice(0, 3);
+
+    //----------------------------------------------------------------------
     // WATER FÖRST
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
     if (waterSegment) {
         var water = new waterSegment();
@@ -77,13 +84,13 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
         x = waterResult.endX;
     }
 
-    //--------------------------------------------------------------------------
-    // ALLA SEGMENT EXACT ONCE
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    // 3 RANDOM SEGMENT (UNIKA)
+    //----------------------------------------------------------------------
 
-    for (var k = 0; k < segments.length; k++) {
+    for (var k = 0; k < chosenSegments.length; k++) {
 
-        var SegmentClass = segments[k];
+        var SegmentClass = chosenSegments[k];
         var segment = new SegmentClass();
 
         var result = segment.ground(this.stage, x);
@@ -97,9 +104,9 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
         x = result.endX;
     }
 
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
     // END SIST
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
     if (endSegment) {
 
@@ -120,22 +127,20 @@ runmysteriet.handler.PlatformHandler.prototype.init = function(levelNumber) {
     console.log("levelWidth:", this.levelWidth);
 };
 
+//------------------------------------------------------------------------------
+// HELPERS
+//------------------------------------------------------------------------------
+
 runmysteriet.handler.PlatformHandler.prototype.addPlatforms = function(platforms) {
-    if (!platforms) {
-        return;
-    }
+    if (!platforms) return;
 
     for (var i = 0; i < platforms.length; i++) {
         this.platforms.push(platforms[i]);
     }
 };
 
-
-
 runmysteriet.handler.PlatformHandler.prototype.addHoles = function(holes) {
-    if (!holes) {
-        return;
-    }
+    if (!holes) return;
 
     for (var i = 0; i < holes.length; i++) {
         this.holes.push(holes[i]);
@@ -143,16 +148,12 @@ runmysteriet.handler.PlatformHandler.prototype.addHoles = function(holes) {
 };
 
 runmysteriet.handler.PlatformHandler.prototype.updateHoles = function(players, onPlayerDead) {
-    if (!players) {
-        return;
-    }
+    if (!players) return;
 
     for (var i = 0; i < players.length; i++) {
         var player = players[i];
 
-        if (!player || player.isDead === true) {
-            continue;
-        }
+        if (!player || player.isDead === true) continue;
 
         for (var j = 0; j < this.holes.length; j++) {
             var hole = this.holes[j];
@@ -161,7 +162,6 @@ runmysteriet.handler.PlatformHandler.prototype.updateHoles = function(players, o
                 if (onPlayerDead) {
                     onPlayerDead(player, i);
                 }
-
                 break;
             }
         }
@@ -169,10 +169,7 @@ runmysteriet.handler.PlatformHandler.prototype.updateHoles = function(players, o
 };
 
 runmysteriet.handler.PlatformHandler.prototype.addEnemySpawns = function(enemySpawns) {
-
-    if (!enemySpawns) {
-        return;
-    }
+    if (!enemySpawns) return;
 
     for (var i = 0; i < enemySpawns.length; i++) {
         this.enemySpawns.push(enemySpawns[i]);
@@ -183,48 +180,33 @@ runmysteriet.handler.PlatformHandler.prototype.getEnemySpawns = function() {
     return this.enemySpawns;
 };
 
-///
 runmysteriet.handler.PlatformHandler.prototype.addWaterAreas = function(waterAreas) {
-    if(!this.waterAreas){
-        this.waterAreas = [];
-    }
-    
+    if (!waterAreas) return;
+
     for (var i = 0; i < waterAreas.length; i++) {
         this.waterAreas.push(waterAreas[i]);
     }
 };
 
-
 runmysteriet.handler.PlatformHandler.prototype.addBoats = function(boats) {
-    
-    if(!this.boats){
-        this.boats = [];
-    }
+    if (!boats) return;
+
     for (var i = 0; i < boats.length; i++) {
         this.boats.push(boats[i]);
     }
 };
 
-
 runmysteriet.handler.PlatformHandler.prototype.startBoatTweens = function(tweens) {
-    var boat = null;
+    if (!this.boats) return;
 
-    if (!this.boats) {
-        console.log("No boat array");
-        return;
-    }
-        console.log("boat count", this.boats.length);
+    console.log("boat count", this.boats.length);
 
     for (var i = 0; i < this.boats.length; i++) {
-        boat = this.boats[i];
+        var boat = this.boats[i];
 
         if (boat && typeof boat.startTween === "function") {
-            boat.startTween(
-                tweens,
-                boat.minX,
-                boat.maxX
-            );
-            console.log("saknar startTween:", boat);
+            boat.startTween(tweens, boat.minX, boat.maxX);
+            console.log("startTween:", boat);
         }
     }
 };
