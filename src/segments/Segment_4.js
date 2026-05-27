0dab76848c7c058a0c6e4ec476c9ccccb6eb49e7
sMusic8 <musicsabina1@gmail.com>
2026-05-28 00:24:02 +0200

    ..

--- src/segments/Segment_4.js
+++ src/segments/Segment_4.js
@@ -355,7 +355,7 @@ runmysteriet.segments.Segment_4.prototype.getArmorPositions = function(segmentSt
     return [
         {
             x: segmentStart + 780,
-            y: this.groundY - 125
+            y: this.groundY - 180
         }
     ];
 };
