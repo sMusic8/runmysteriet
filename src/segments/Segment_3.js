cf237b9cad65e75289a45cb533274eac764b14d8
sMusic8 <musicsabina1@gmail.com>
2026-05-28 00:19:52 +0200

    3

--- src/segments/Segment_3.js
+++ src/segments/Segment_3.js
@@ -289,7 +289,7 @@ runmysteriet.segments.Segment_3.prototype.getDiseasePositions = function(segment
         {
             type: "brown",
             x: segmentStart + 460,
-            y: this.groundY - 50
+            y: this.groundY - 150
         },
         {
             type: "red",
